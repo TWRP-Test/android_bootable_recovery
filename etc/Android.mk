299c86da14422fbbd2153daa71d427d12593fe8b
adontoo <1056651908@qq.com>
2025-12-11 21:49:20 +0800

    Add static ttyd binaries

--- etc/Android.mk
+++ etc/Android.mk
@@ -155,6 +155,16 @@ ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS),true)
 	include $(BUILD_PREBUILT)
 endif
 
+ifneq ($(TW_NO_NETWORK), true)
+	include $(CLEAR_VARS)
+	LOCAL_MODULE := ttyd.rc
+	LOCAL_MODULE_TAGS := optional
+	LOCAL_MODULE_CLASS := EXECUTABLES
+	LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/etc/init
+	LOCAL_SRC_FILES := init/$(LOCAL_MODULE)
+	include $(BUILD_PREBUILT)
+endif
+
 ifeq ($(TW_INCLUDE_OMAPI), true)
 	include $(CLEAR_VARS)
 	LOCAL_MODULE := se_omapi.rc
