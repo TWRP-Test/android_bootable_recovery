39f11f215afb2cc3c228a7f463ef69f220796898
YuKongA <70465933+YuKongA@users.noreply.github.com>
2026-08-23 14:53:39 +0800

    minadbd: drop obsolete SDK 23 compatibility

--- minadbd/minadbd.cpp
+++ minadbd/minadbd.cpp
@@ -68,8 +68,8 @@ int main(int argc, char** argv) {
 
   usb_init();
 
-    //VLOG(ADB) << "Event loop starting";
-    fdevent_loop();
+  VLOG(ADB) << "Event loop starting";
+  fdevent_loop();
 
   return 0;
 }
