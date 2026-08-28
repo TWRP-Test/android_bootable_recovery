72c8c1fb2c288ae869ae9b896c8478b0fcf6e117
YuKongA <70465933+YuKongA@users.noreply.github.com>
2026-08-28 15:30:06 +0800

    recovery: Fix Soong variable quoting and warnings

--- soong/makevars.go
+++ soong/makevars.go
@@ -2,13 +2,17 @@ package twrp
 
 import (
 	"android/soong/android"
+	"strings"
 )
 
 func getMakeVars(ctx android.BaseContext, mVar string) string {
 	makeVars := ctx.Config().VendorConfig("twrpVarsPlugin")
 	var makeVar = ""
 	if makeVars.IsSet(mVar) {
-		makeVar = makeVars.String(mVar)
+		makeVar = strings.TrimSpace(makeVars.String(mVar))
+		if len(makeVar) >= 2 && makeVar[0] == '"' && makeVar[len(makeVar)-1] == '"' {
+			makeVar = makeVar[1 : len(makeVar)-1]
+		}
 	}
 	return makeVar
 }
